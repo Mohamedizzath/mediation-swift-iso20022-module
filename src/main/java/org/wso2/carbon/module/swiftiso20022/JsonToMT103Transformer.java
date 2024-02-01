c9ba8a630d85ccc45435636b8524e9dcca0c66e4
Sineth Thamuditha <sinetht@wso2.com>
2024-02-01 10:14:00 +0530

    Initialize project structure for JSON to MT103 transformer. Add component.xml and json-to-mt103.xml with configurations. Add class mediator and payload factory mediator to the sequence.

---
+++ src/main/java/org/wso2/carbon/module/swiftiso20022/JsonToMT103Transformer.java
@@ -0,0 +1,16 @@
+package org.wso2.carbon.module.swiftiso20022;
+
+import org.apache.synapse.MessageContext;
+import org.wso2.carbon.connector.core.AbstractConnector;
+import org.wso2.carbon.connector.core.ConnectException;
+
+
+/**
+ * Class to convert JSON Object to MT103 format.
+ */
+public class JsonToMT103Transformer extends AbstractConnector {
+    @Override
+    public void connect(MessageContext messageContext) throws ConnectException {
+        // TODO: Implement validation logic
+    }
+}
