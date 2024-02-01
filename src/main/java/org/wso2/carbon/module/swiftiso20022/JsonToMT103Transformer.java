273cf653597d9afecb8898455cda92654bb603e4
Sineth Thamuditha <sinetht@wso2.com>
2024-02-01 10:35:12 +0530

    Add logic to get request payload and handle request payload missing error.

--- src/main/java/org/wso2/carbon/module/swiftiso20022/JsonToMT103Transformer.java
+++ src/main/java/org/wso2/carbon/module/swiftiso20022/JsonToMT103Transformer.java
@@ -1,8 +1,12 @@
 package org.wso2.carbon.module.swiftiso20022;
 
 import org.apache.synapse.MessageContext;
+import org.apache.synapse.core.axis2.Axis2MessageContext;
 import org.wso2.carbon.connector.core.AbstractConnector;
 import org.wso2.carbon.connector.core.ConnectException;
+import org.wso2.carbon.module.swiftiso20022.utils.ConnectorUtils;
+
+import java.util.Optional;
 
 
 /**
@@ -11,6 +15,20 @@ import org.wso2.carbon.connector.core.ConnectException;
 public class JsonToMT103Transformer extends AbstractConnector {
     @Override
     public void connect(MessageContext messageContext) throws ConnectException {
-        // TODO: Implement validation logic
+        try {
+            org.apache.axis2.context.MessageContext axis2MessageContext = ((Axis2MessageContext) messageContext)
+                    .getAxis2MessageContext();
+            Optional<String> payload = ConnectorUtils.buildMessagePayloadFromMessageContext(axis2MessageContext);
+            if (payload.isPresent()) {
+                // TODO: Implement validation logic
+            } else {
+                // TODO: replace with constants
+                this.log.error("Request payload is missing.");
+            }
+        } catch (Exception e) {
+            // TODO: add exception handling logic
+            this.log.error(e.getMessage());
+            throw new ConnectException(e);
+        }
     }
 }
