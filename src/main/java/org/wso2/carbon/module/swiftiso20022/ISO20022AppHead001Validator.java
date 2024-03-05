b80893cafb1cf2117bce651a7ff5ea7d6c9fa437
mohamedizzath <mohamedizzath2016@gmail.com>
2024-03-05 13:47:53 +0530

    Update log of exceptions with message

--- src/main/java/org/wso2/carbon/module/swiftiso20022/ISO20022AppHead001Validator.java
+++ src/main/java/org/wso2/carbon/module/swiftiso20022/ISO20022AppHead001Validator.java
@@ -60,14 +60,14 @@ public class ISO20022AppHead001Validator extends AbstractConnector {
             String errMsg = String.format("%s, Line number: %d, Column number: %d",
                     e.getMessage(), e.getLineNumber(), e.getColumnNumber());
 
-            this.log.error(e);
+            this.log.error(e.getMessage());
             ConnectorUtils.appendErrorToMessageContext(messageContext,
                     ConnectorConstants.ERROR_INVALID_ISO_HEAD001_XML_MSG,
                     errMsg);
 
             throw new ConnectException(e, errMsg);
         } catch (SAXException | JaxenException | IOException e) {
-            this.log.error(e);
+            this.log.error(e.getMessage());
             ConnectorUtils.appendErrorToMessageContext(messageContext,
                     ConnectorConstants.ERROR_VALIDATING_XML, e.getMessage());
 
