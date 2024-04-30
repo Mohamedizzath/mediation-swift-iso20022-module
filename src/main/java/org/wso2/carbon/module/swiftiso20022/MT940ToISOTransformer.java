b6eabaea5d8f7a209146b55f6f22cd80d4e80616
mohamedizzath <mohamedizzath2016@gmail.com>
2024-04-30 14:42:17 +0530

    Add initial connector code

---
+++ src/main/java/org/wso2/carbon/module/swiftiso20022/MT940ToISOTransformer.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2024, WSO2 LLC. (https://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.module.swiftiso20022;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.synapse.MessageContext;
+import org.apache.synapse.util.PayloadHelper;
+import org.wso2.carbon.connector.core.AbstractConnector;
+import org.wso2.carbon.connector.core.ConnectException;
+import org.wso2.carbon.module.swiftiso20022.exceptions.MTMessageParsingException;
+import org.wso2.carbon.module.swiftiso20022.mt.models.messages.MT940Message;
+import org.wso2.carbon.module.swiftiso20022.mt.parsers.MT940Parser;
+
+/**
+ * Transform MT940 to ISO20022.camt.053.
+ */
+public class MT940ToISOTransformer extends AbstractConnector {
+    private static Log log = LogFactory.getLog(MT940ToISOTransformer.class);
+
+    @Override
+    public void connect(MessageContext messageContext) throws ConnectException {
+        try {
+            String message = PayloadHelper.getTextPayload(messageContext);
+
+            MT940Message mt940Message = MT940Parser.parse(message);
+
+        } catch (MTMessageParsingException e) {
+            throw new ConnectException(e);
+        }
+    }
+}
