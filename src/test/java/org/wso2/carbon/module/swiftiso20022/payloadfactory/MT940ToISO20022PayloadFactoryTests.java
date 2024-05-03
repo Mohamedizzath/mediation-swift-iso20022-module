4095f526fd7d4ac8c80b75195c49d2ead1627135
mohamedizzath <mohamedizzath2016@gmail.com>
2024-05-03 15:47:09 +0530

    Add initial test code

---
+++ src/test/java/org/wso2/carbon/module/swiftiso20022/payloadfactory/MT940ToISO20022PayloadFactoryTests.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2024, WSO2 LLC. (https://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.module.swiftiso20022.payloadfactory;
+
+import com.google.gson.Gson;
+import freemarker.template.Template;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.wso2.carbon.module.swiftiso20022.mt.models.messages.MT940Message;
+import org.wso2.carbon.module.swiftiso20022.utils.FreemarkerTemplateTestUtils;
+import org.wso2.carbon.module.swiftiso20022.utils.ISO20022ToMT940PayloadFactoryTestConstants;
+
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.Map;
+
+/**
+ * Test class for validating payload factory freemarker template for
+ * convert MT940 message to ISO20022 message.
+ */
+public class MT940ToISO20022PayloadFactoryTests {
+
+    private static final Gson gson = new Gson();
+
+    @Test
+    public void testValidMT940MessageScenario(String mt940Message, String isoMessage) throws Exception {
+        // Get the ftl file as input stream
+        InputStream freemarkerTempStream = getClass().getClassLoader()
+                .getResourceAsStream("freemarkerTemplates/MT940ToISO.ftl");
+
+        // Create the map from the MT940 JSON
+        Map<String, Object> mt940Map = gson.fromJson(mt940Message, Map.class);
+
+        Template template = FreemarkerTemplateTestUtils
+                .getFreemarkerTemplate(freemarkerTempStream, "ISOToMT940");
+
+        Writer stringwriter = new StringWriter();
+        template.process(mt940Map, stringwriter);
+        String freemarkerOutput = stringwriter.toString().replace("\r", "");
+
+        Assert.assertEquals(freemarkerOutput, isoMessage);
+
+    }
+}
---
+++ src/test/java/org/wso2/carbon/module/swiftiso20022/utils/MT940ToISO20022PayloadFactoryTestConstants.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2024, WSO2 LLC. (https://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.module.swiftiso20022.utils;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Contains test constants for testing payload factory mediator for
+ * converting MT940 message to ISO20022.
+ */
+public class MT940ToISO20022PayloadFactoryTestConstants {
+}
